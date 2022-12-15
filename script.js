822b28fafbe2eb5774a4c0d28bbf155b6e5a38a1
TheUnknownMonkey <miha.puc@gmail.com>
2022-12-15 23:08:47 +0100

    stuck

---
+++ script.js
@@ -0,0 +1,87 @@
+const openModal = document.getElementById("open-modal");
+const addBook = document.getElementById("submit-book");
+const books = document.getElementById("books");
+const book = document.createElement("div");
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const pagesInput = document.getElementById("pages");
+const form = document.getElementById("form");
+let library = [];
+let booksDiv= [];
+let = garbage = [];
+
+
+openModal.addEventListener("click", ()=>{
+    form.style.display="flex";
+    
+   
+
+})
+
+
+addBook.addEventListener("click", ()=>{
+    books.innerHTML="";
+   
+    let currentBook= {
+        "title":titleInput.value,
+        "author":authorInput.value,
+        "number":pagesInput.value
+    }
+    library.push(currentBook);
+    form.style.display="none";
+    loadBooks()
+    // for(let i=0; i<booksDiv.length;i++){
+
+    // }
+   
+})
+
+function loadBooks(){
+    
+    for (let i=0;i<library.length; i++){
+        //book
+        let ThisBook = document.createElement("div");
+        ThisBook.classList.add("book")
+        let title = document.createElement("h3");
+        let author = document.createElement("h3");
+        let pages = document.createElement("h3");
+        let button = document.createElement("button");
+        //text in the book
+        title.innerHTML=library[i]["title"];
+        
+        author.innerHTML=library[i]["author"];
+        
+        pages.innerHTML=library[i]["number"];
+        
+        button.innerHTML = "Delete";
+        //append text to book
+        ThisBook.appendChild(title);
+        ThisBook.appendChild(button)
+        //append book to the section
+        booksDiv.push(ThisBook);
+        books.appendChild(ThisBook);
+ 
+    }
+    for (let j=0; j<books.childNodes.length;j++){
+      
+        books.childNodes[j].childNodes[1].addEventListener("click", ()=>{
+            console.log(j)
+            console.log(books.childNodes[0]);
+
+            books.removeChild(books.childNodes[j]);
+            library.splice(j, 1);
+      
+            j=books.childNodes.length
+        }, false)
+    }
+
+        // }
+        // button.addEventListener("click", ()=>{
+        //     library[i].pop;
+        //     books.removeChild(ThisBook)
+            
+        // })
+   
+    
+    
+}
